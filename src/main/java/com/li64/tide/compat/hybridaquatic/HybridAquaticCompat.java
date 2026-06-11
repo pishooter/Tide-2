2e377245156478b8f4f9a8274b85a46bf0305555
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-06-11 12:56:26 +0300

    Changed class names to match new class names in Hybrid Aquatic

--- src/main/java/com/li64/tide/compat/hybridaquatic/HybridAquaticCompat.java
+++ src/main/java/com/li64/tide/compat/hybridaquatic/HybridAquaticCompat.java
@@ -1,10 +1,10 @@
 package com.li64.tide.compat.hybridaquatic;
 
 import com.li64.tide.registries.entities.misc.fishing.TideFishingHook;
-import dev.hybridlabs.aquatic.entity.HybridAquaticEntityTypes;
+import dev.hybridlabs.aquatic.entity.HAEntityTypes;
 import dev.hybridlabs.aquatic.entity.fish.StingrayEntity;
 import dev.hybridlabs.aquatic.entity.fish.StingrayEntity.Companion.Type;
-import dev.hybridlabs.aquatic.item.HybridAquaticItems;
+import dev.hybridlabs.aquatic.item.HAItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.server.level.ServerLevel;
@@ -21,12 +21,12 @@ public class HybridAquaticCompat {
         Entity newEntity = null;
         ItemStack hookItem = hook.getHook();
 
-        if (hookItem.is(HybridAquaticItems.INSTANCE.getOMINOUS_HOOK().get())) {
-            var karkinosType = HybridAquaticEntityTypes.INSTANCE.getKARKINOS().get();
+        if (hookItem.is(HAItems.INSTANCE.getOMINOUS_HOOK().get())) {
+            var karkinosType = HAEntityTypes.INSTANCE.getKARKINOS().get();
             newEntity = createAndLaunchEntityAtPlayer(karkinosType, player, hook.blockPosition());
             if (newEntity != null) hook.clearHookItem();
         }
-        if (hookItem.is(HybridAquaticItems.INSTANCE.getCREEPERMAGNET_HOOK().get())) {
+        if (hookItem.is(HAItems.INSTANCE.getCREEPERMAGNET_HOOK().get())) {
             var creeperType = EntityType.CREEPER;
             newEntity = createAndLaunchEntityAtPlayer(creeperType, player, hook.blockPosition());
             if (newEntity != null) hook.clearHookItem();
